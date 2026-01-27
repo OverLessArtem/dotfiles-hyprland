bf8f8525f25ce3942521581d57cd7203e517a0ba
OverLessArtem <Artemfreedom@tuta.io>
2026-01-27 17:32:15 +0500

    Updated hyprland config

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Copying files
-cp -r alacritty ~/.config
 cp -r btop ~/.config
 cp -r fuzzel ~/.config
 cp -r hypr ~/.config
 cp -r swaync ~/.config
 cp -r waybar ~/.config
 cp -r yazi ~/.config
+cp -r kitty ~/.config
 
 
 echo "Done!"
